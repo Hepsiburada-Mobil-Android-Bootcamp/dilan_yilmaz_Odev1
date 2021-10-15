dde377e26f2b06a7171c2bb65bd74449dad27eb1
Dilan Yılmaz <dilanyilmaz1997@gmail.com>
2021-10-15 16:33:23 +0300

    login sayfası eklendi

--- app/src/main/java/com/example/hepsiburadaodev1/activities/LoginScreen.kt
+++ app/src/main/java/com/example/hepsiburadaodev1/activities/LoginScreen.kt
@@ -1,14 +1,23 @@
 package com.example.hepsiburadaodev1.activities
 
+import android.graphics.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CalendarContract
+import android.widget.TextView
+import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
 import com.example.hepsiburadaodev1.R
 
 class LoginScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
-
+        val textView = findViewById<TextView>(R.id.foodninja)
+        val textView2 = findViewById<TextView>(R.id.textView7)
+        val shader = LinearGradient(0f, 0f, 0f, textView.textSize,Color.parseColor("#53E88B"), Color.parseColor("#15BE77"), Shader.TileMode.CLAMP)
+        textView.paint.shader = shader
+        textView2.paint.shader = shader
     }
 
 }
