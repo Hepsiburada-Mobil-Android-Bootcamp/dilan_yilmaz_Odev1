0e513ef7211e8cfb286c62604359dc5beff7652e
Dilan Yılmaz <dilanyilmaz1997@gmail.com>
2021-10-15 18:19:12 +0300

    livedata eklendi

--- app/src/main/java/com/example/hepsiburadaodev1/activities/MainActivity.kt
+++ app/src/main/java/com/example/hepsiburadaodev1/activities/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.example.hepsiburadaodev1.activities
 
+
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.hepsiburadaodev1.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+    private lateinit var textView: TextView
+    private lateinit var liveData: LiveData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageView = findViewById(R.id.imageView)
+        textView = findViewById(R.id.textView)
+        liveData()
+
+    }
+    private fun liveData() {
+        liveData = LiveData(application)
+        liveData.observe(this) { isConnected ->
+            if (isConnected) {
+                imageView.setImageResource(R.drawable.wifi_connect)
+
+                textView.setTextColor(Color.parseColor("#4CAF50"))
+            } else {
+                imageView.setImageResource(R.drawable.wifi_disconnected)
+
+                textView.setTextColor(Color.parseColor("#F44336"))
+            }
+        }
+
     }
 }
